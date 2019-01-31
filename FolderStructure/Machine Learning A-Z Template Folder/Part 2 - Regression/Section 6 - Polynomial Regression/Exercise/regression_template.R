7c4f5f869db0d8b0d0d519a23ab055a5144dc356
schoudh5 <schoudh5@asu.edu>
2019-01-31 10:41:10 +0530

    Finishing off regression"

---
+++ FolderStructure/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/Exercise/regression_template.R
@@ -0,0 +1,46 @@
+# Data Preprocessing Template
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+
+dataset = dataset[2:3]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+
+# fitting polynomial regression to the dataset
+# create our regressor right here
+
+# predicting a new result with Regression Model
+y_pred = predict(regressor, data.frame(Level=6.5))
+
+# Visualizing the Regression Model results
+
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary), color = 'red')+
+  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)), color = 'blue')+
+  ggtitle('Truth or Bluff(Regression Model)')+
+  xlab('Level')+
+  ylab('Salary')
+
+# Visualizing the Regression Model results ( for higher resultion) and smoother curve
+
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level),0.1)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary), color = 'red')+
+  geom_line(aes(x =x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))), color = 'blue')+
+  ggtitle('Truth or Bluff(Regression Model)')+
+  xlab('Level')+
+  ylab('Salary')
--- FolderStructure/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/Exercise/regression_template.py
+++ FolderStructure/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/Exercise/regression_template.py
@@ -1,8 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 29 10:17:20 2019
+# Polynomial Regression
 
-@author: master
-"""
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# TODO : splitting the dataset and test set if needed
+
+# TODO : apply feature scaling if needed
+# create the regression here
+
+# fitting the regression model to the dataset
+
+
+# predicting a new result with Polynomial regression
+y_pred = regressor.predict((6.5))
+
+# visualizing the regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff(Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+# visualizing the regression results for higher resolution and smoother curve
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff(Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+
+
+
 
