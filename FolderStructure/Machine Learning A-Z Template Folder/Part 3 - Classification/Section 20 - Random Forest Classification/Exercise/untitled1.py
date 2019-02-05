c4aca0a10d1cc06d892921ca50d39cbe8fc4839f
schoudh5 <schoudh5@asu.edu>
2019-02-05 09:12:59 +0530

    Commit before reinforcement learning

---
+++ FolderStructure/Machine Learning A-Z Template Folder/Part 3 - Classification/Section 20 - Random Forest Classification/Exercise/untitled1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  2 22:33:57 2019
+
+@author: master
+"""
+# importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+# importing the mall dataset with [pandas]
+dataset = pd.read_csv('mall.csv')
---
+++ FolderStructure/Machine Learning A-Z Template Folder/Part 4 - Clustering/Section 25 - Hierarchical Clustering/Exercise/hc.py
@@ -0,0 +1,40 @@
+# Hierarchial clustering
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the mall dataset with pandas
+dataset = pd.read_csv('Mall_Customers.csv') 
+X = dataset.iloc[:, [3,4]].values
+
+# TODO : uncomment dendogram code for finding the number of clusters
+# using the dendogram to find the optimal number of clusters
+#import scipy.cluster.hierarchy as sch
+#
+#dendogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+#plt.title('Dendogram')
+#plt.xlabel('Customers')
+#plt.ylabel('Euclidean distance')
+#plt.show()
+# fitting hierarchial clusering to the mall dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity="euclidean", linkage='ward')
+y_hc = hc.fit_predict(X)
+
+# visualizings clusters
+
+# Visualizing the clusters
+plt.scatter(X[y_hc ==0,0], X[y_hc==0,1], s=100, c='red', label = 'Careful')
+plt.scatter(X[y_hc ==1,0], X[y_hc==1,1], s=100, c='blue', label = 'Standard')
+plt.scatter(X[y_hc ==2,0], X[y_hc==2,1], s=100, c='green', label = 'Target')
+plt.scatter(X[y_hc ==3,0], X[y_hc==3,1], s=100, c='cyan', label = 'Careless')
+plt.scatter(X[y_hc ==4,0], X[y_hc==4,1], s=100, c='magenta', label = 'Sensible')
+
+plt.title('Cluster of clients')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending score (1-100)')
+plt.legend()
+plt.show()
+
